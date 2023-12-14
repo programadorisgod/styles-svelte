ab484ff0f2d35d7562b16eb8fcd7d6f40123475d
programadorisgod <catapiascastro@gmail.com>
2023-12-13 19:05:32 -0500

    -mCanvasConfetti

---
+++ src/App.svelte
@@ -0,0 +1,44 @@
+<script>
+  import Input from "./Input.svelte";
+
+ let name = "John";
+ let lastname = "Doe";
+
+</script>
+
+<main class="main_app">
+   <h1 class="title_app">Hi! {name}</h1>
+   
+   <fieldset class="fieldset_app">
+   <Input  id="name"  label="Name" bind:value={name}  />
+    <Input id="lastname" label="Lastname" bind:value={lastname} />
+   </fieldset>
+
+   <span class="span_main_app">You are <strong> {name} {lastname} </strong> </span>
+</main>
+
+<style>
+
+  .title_app{
+    color: #000;
+    margin-bottom: 1.5em;
+  }
+ 
+  .fieldset_app{
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    margin-bottom: 1.5em;
+    width: 100%;
+    height: 200px;
+    border-radius: 10px;
+    border: 1px solid #ccc;
+    box-shadow: 0 0 10px #ccc;
+  }
+
+  .span_main_app{
+    color: #000;
+  }
+
+</style>
---
+++ src/Input.svelte
@@ -0,0 +1,42 @@
+<script>
+
+    export let value;
+    export let label;
+    export let id;
+   
+
+</script>
+
+
+
+<label class="label_input" for={id}>{label}</label>
+<input type="text" bind:value id={id}  class:invalid={value.length === 0} class="input_app">
+
+
+
+
+<style>
+  
+ .label_input{
+    margin: 0.5em 0 0.5em 0;
+    color: #000;
+ }
+
+ .input_app{
+    width: 90%;
+    height: 20px;
+    border-radius: 10px;
+    border: 1px solid #ccc;
+    background-color: #ccc;
+    color: #000;
+    outline: none;
+    padding: 0.2em;
+
+ }
+
+
+ .invalid  {
+    background-color: #f11111;
+    
+ }
+</style>
